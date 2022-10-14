c560814c20fff9202d9591539c1deb2c152c76bc
Krzysztof Czerwonopolski <83087348+JanPalmer@users.noreply.github.com>
2022-10-14 11:06:28 +0200

    ver. 14102022, 1106

---
+++ PRO1/PRO1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace PRO1
+{
+    partial class CGP1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panelControl = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonNewPolygon = new System.Windows.Forms.Button();
+            this.buttonClearSpace = new System.Windows.Forms.Button();
+            this.groupBoxAlgorithm = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.buttonDebug = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.buttonLMB = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panelControl.SuspendLayout();
+            this.groupBoxAlgorithm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 154F));
+            this.tableLayoutPanel1.Controls.Add(this.panelControl, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.canvas, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 511);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panelControl
+            // 
+            this.panelControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelControl.ColumnCount = 1;
+            this.panelControl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.panelControl.Controls.Add(this.buttonNewPolygon, 0, 0);
+            this.panelControl.Controls.Add(this.buttonClearSpace, 0, 1);
+            this.panelControl.Controls.Add(this.buttonDebug, 0, 2);
+            this.panelControl.Controls.Add(this.groupBoxAlgorithm, 0, 4);
+            this.panelControl.Controls.Add(this.buttonLMB, 0, 3);
+            this.panelControl.Location = new System.Drawing.Point(633, 3);
+            this.panelControl.Name = "panelControl";
+            this.panelControl.RowCount = 5;
+            this.panelControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.85714F));
+            this.panelControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.14286F));
+            this.panelControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.panelControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 181F));
+            this.panelControl.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 127F));
+            this.panelControl.Size = new System.Drawing.Size(148, 505);
+            this.panelControl.TabIndex = 0;
+            // 
+            // buttonNewPolygon
+            // 
+            this.buttonNewPolygon.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonNewPolygon.Location = new System.Drawing.Point(3, 3);
+            this.buttonNewPolygon.Name = "buttonNewPolygon";
+            this.buttonNewPolygon.Size = new System.Drawing.Size(142, 54);
+            this.buttonNewPolygon.TabIndex = 1;
+            this.buttonNewPolygon.Text = "New Polygon";
+            this.buttonNewPolygon.UseVisualStyleBackColor = true;
+            this.buttonNewPolygon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonNewPolygon_MouseClick);
+            // 
+            // buttonClearSpace
+            // 
+            this.buttonClearSpace.Location = new System.Drawing.Point(3, 63);
+            this.buttonClearSpace.Name = "buttonClearSpace";
+            this.buttonClearSpace.Size = new System.Drawing.Size(142, 57);
+            this.buttonClearSpace.TabIndex = 2;
+            this.buttonClearSpace.Text = "Clear Drawing Space";
+            this.buttonClearSpace.UseVisualStyleBackColor = true;
+            this.buttonClearSpace.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonClearSpace_MouseClick);
+            // 
+            // groupBoxAlgorithm
+            // 
+            this.groupBoxAlgorithm.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxAlgorithm.Controls.Add(this.radioButton2);
+            this.groupBoxAlgorithm.Controls.Add(this.radioButton1);
+            this.groupBoxAlgorithm.Location = new System.Drawing.Point(3, 380);
+            this.groupBoxAlgorithm.Name = "groupBoxAlgorithm";
+            this.groupBoxAlgorithm.Size = new System.Drawing.Size(142, 122);
+            this.groupBoxAlgorithm.TabIndex = 0;
+            this.groupBoxAlgorithm.TabStop = false;
+            this.groupBoxAlgorithm.Text = "Current Edge Drawing Algorithm";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 75);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(94, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 39);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // buttonDebug
+            // 
+            this.buttonDebug.Location = new System.Drawing.Point(3, 144);
+            this.buttonDebug.Name = "buttonDebug";
+            this.buttonDebug.Size = new System.Drawing.Size(142, 30);
+            this.buttonDebug.TabIndex = 3;
+            this.buttonDebug.Text = "button1";
+            this.buttonDebug.UseVisualStyleBackColor = true;
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.White;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(3, 3);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(624, 505);
+            this.canvas.TabIndex = 1;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // buttonLMB
+            // 
+            this.buttonLMB.Location = new System.Drawing.Point(3, 199);
+            this.buttonLMB.Name = "buttonLMB";
+            this.buttonLMB.Size = new System.Drawing.Size(142, 21);
+            this.buttonLMB.TabIndex = 4;
+            this.buttonLMB.Text = "button1";
+            this.buttonLMB.UseVisualStyleBackColor = true;
+            // 
+            // CGP1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 511);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(650, 400);
+            this.Name = "CGP1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Computer Graphics Project 1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panelControl.ResumeLayout(false);
+            this.groupBoxAlgorithm.ResumeLayout(false);
+            this.groupBoxAlgorithm.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel panelControl;
+        private System.Windows.Forms.GroupBox groupBoxAlgorithm;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button buttonNewPolygon;
+        private System.Windows.Forms.Button buttonClearSpace;
+        private System.Windows.Forms.Button buttonDebug;
+        private System.Windows.Forms.Button buttonLMB;
+    }
+}
+
---
+++ PRO1/PRO1/Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRO1
+{
+    public partial class CGP1 : Form
+    {
+        public const int radius = 10, edgeThickness = 2;
+        public Rectangle rec;
+        public Pen penVertex;
+        public Pen penEdge, penEdgeHighlight, penEdgeConstLength, penEdgeRelation;
+        public Brush brushVertex = Brushes.Black;
+        public Brush brushVertexHighlight = Brushes.DarkOrange;
+        public Brush brushVertexSelected = Brushes.Red;
+        public Brush brushEdge = Brushes.Black;
+        public Brush brushEdgeHighlight = Brushes.Orange;
+        public Brush brushEdgeConstLength = Brushes.Green;
+        public Brush brushEdgeRelation = Brushes.OrangeRed;
+        public Graphics graph;
+        public Bitmap drawArea;
+
+        public enum AppState
+        {
+            ready,
+            newPolygonBegin,
+            newPolygonDrawing
+        }
+        public class Vertex
+        {
+            public Point point;
+            public Brush brush { get; set; }
+            public void ChangePlacement(Point _p)
+            {
+                point = _p;
+            }
+            public void ChangePlacement(int _X, int _Y)
+            {
+                point.X = _X;
+                point.Y = _Y;
+            }
+            public Vertex(Point _p, Brush _brush)
+            {
+                point = _p;
+                brush = _brush;
+            }
+        }
+        public class Polygon
+        {
+            public List<Vertex> vertices;
+
+            public void SetVertexBrush(Brush _brush)
+            {
+                foreach(Vertex v in this.vertices)
+                {
+                    v.brush = _brush;
+                }
+            }
+        }
+
+        public AppState state;
+        public Polygon tempPolygon;
+        public List<Polygon> polygons;
+
+        public Vertex selectedVertex = null;
+        public bool isPressedLMB = false;
+
+        public CGP1()
+        {
+            InitializeComponent();
+            rec = new Rectangle(0, 0, radius * 2, radius * 2);
+            penVertex = new Pen(brushVertex);
+            penEdge = new Pen(brushEdge, edgeThickness);
+            penEdgeHighlight = new Pen(brushEdgeHighlight, edgeThickness);
+
+            polygons = new List<Polygon>();
+            drawArea = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            canvas.Image = drawArea;
+            Graphics g = Graphics.FromImage(drawArea);
+            g.Clear(Color.White);
+            g.Dispose();
+
+            state = AppState.ready;
+        }
+
+        private void buttonNewPolygon_SetState_Ready()
+        {
+            buttonNewPolygon.BackColor = SystemColors.Control;
+            buttonNewPolygon.Text = "New Polygon";
+        }
+
+        private void buttonNewPolygon_SetState_newPolygon()
+        {
+            buttonNewPolygon.BackColor = SystemColors.GradientActiveCaption;
+            buttonNewPolygon.Text = "Cancel Creation";
+        }
+
+        private void SetState_Ready()
+        {
+            tempPolygon = null;
+            canvas.Invalidate();
+            state = AppState.ready;
+            buttonNewPolygon_SetState_Ready();
+        }
+        private void SetState_newPolygon()
+        {
+            state = AppState.newPolygonBegin;
+            buttonNewPolygon_SetState_newPolygon();
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                switch (state)
+                {
+                    case AppState.newPolygonBegin:
+                        SetState_Ready();
+                        break;
+                    case AppState.newPolygonDrawing:
+                        tempPolygon.vertices.RemoveAt(tempPolygon.vertices.Count - 1);
+                        if (tempPolygon.vertices.Count <= 0)
+                        {
+                            SetState_Ready();
+                        }
+                        else
+                        {
+                            canvas.Invalidate();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                return;
+            }
+
+
+            if (e.Button == MouseButtons.Left)
+            {
+                Point position = new Point(e.X, e.Y);
+                switch (state)
+                {
+                    case AppState.newPolygonBegin:
+                        tempPolygon = new Polygon();
+                        tempPolygon.vertices = new List<Vertex>();
+                        tempPolygon.vertices.Add(new Vertex(position, brushVertexHighlight));
+                        //DrawVertex(v);
+                        canvas.Invalidate();
+                        state = AppState.newPolygonDrawing;
+                        break;
+                    case AppState.newPolygonDrawing:
+                        int dx = position.X - tempPolygon.vertices[0].point.X;
+                        int dy = position.Y - tempPolygon.vertices[0].point.Y;
+
+                        // If most recent click is within the first vertex of the polygon, finish drawing polygon
+                        if (dx * dx + dy * dy < 2 * radius * radius)
+                        {
+                            if (tempPolygon.vertices.Count > 2)
+                            {
+                                tempPolygon.SetVertexBrush(brushVertex);
+                                polygons.Add(tempPolygon);                                
+                                SetState_Ready();
+                            }
+                        }
+                        else
+                        {
+                            foreach (Polygon p in polygons)
+                            {
+                                foreach (Vertex v in p.vertices)
+                                {
+                                    dx = position.X - v.point.X;
+                                    dy = position.Y - v.point.Y;
+                                    if (dx * dx + dy * dy < 4 * radius * radius) return;
+                                }
+                            }
+                            foreach (Vertex v in tempPolygon.vertices)
+                            {
+                                dx = position.X - v.point.X;
+                                dy = position.Y - v.point.Y;
+                                if (dx * dx + dy * dy < 4 * radius * radius) return;
+                            }
+
+                            //Vertex newVertex = new Vertex(position);
+                            tempPolygon.vertices.Add(new Vertex(position, brushVertexHighlight));
+                            //DrawVertex(newVertex);
+                            //DrawEdge(tempPolygon.vertices[tempPolygon.vertices.Count - 2], newVertex);
+                        }
+                        canvas.Invalidate();
+                        break;
+                    case AppState.ready:
+                        selectedVertex = null;
+                        foreach (Polygon p in polygons)
+                        {
+                            foreach (Vertex v in p.vertices)
+                            {
+                                dx = position.X - v.point.X;
+                                dy = position.Y - v.point.Y;
+                                if (dx * dx + dy * dy < 2 * radius * radius)
+                                {
+                                    selectedVertex = v;
+                                    break;
+                                }
+                            }
+                            if (selectedVertex != null) break;
+                        }
+
+                        if (selectedVertex == null)
+                        {
+                            buttonDebug.Text = "No vertex selected";
+                            return;
+                        }
+
+                        buttonDebug.Text = "Vertex selected";
+
+                        selectedVertex.brush = brushVertexSelected;
+                        isPressedLMB = true;
+                        buttonLMB.Text = "LMB true";
+                        break;
+                    default:
+                        break;
+                }
+
+                return;
+            }
+        }
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (isPressedLMB == true)
+                {
+                    buttonDebug.Text = "Vertex unselected";
+                    isPressedLMB = false;
+                    selectedVertex.brush = brushVertex;
+                    selectedVertex = null;
+                    canvas.Invalidate();
+                    buttonLMB.Text = "LMB false";
+                }
+            }
+        }
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            while (isPressedLMB == true && canvas.Focused)
+            {
+                buttonDebug.Text = "Vertex shmovin'";
+                canvas.Invalidate();
+                selectedVertex.ChangePlacement(e.Location);
+                canvas.Invalidate();
+            }
+        }
+
+        private void buttonNewPolygon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+                if(state == AppState.ready)
+                {
+                    SetState_newPolygon();
+                }
+                else
+                {
+                    SetState_Ready();
+                }
+        }
+
+        private void buttonClearSpace_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                polygons.Clear();
+                SetState_Ready();
+            }
+        }
+
+        public void DrawVertex(Vertex v)
+        {
+            if (v == null) return;
+            rec.Location = new System.Drawing.Point(v.point.X - radius, v.point.Y - radius);
+            graph.FillEllipse(brushVertex, rec);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            canvas.Image.Dispose();
+        }
+
+        public void DrawEdge(Vertex vp, Vertex vk)
+        {
+            if (vp == null || vk == null) return;
+            graph.DrawLine(penEdge, vp.point, vk.point);
+        }
+        public void DrawPolygon(Polygon p)
+        {
+            if (p == null) return;
+            foreach (Vertex v in p.vertices)
+            {
+                DrawVertex(v);
+            }
+
+            for (int i = 0; i < p.vertices.Count; i++)
+            {
+                DrawEdge(p.vertices[i], p.vertices[(i + 1) % p.vertices.Count]);
+            }
+        }
+        public void DrawUnfinishedPolygon(Polygon p)
+        {
+            if (p == null) return;
+
+            foreach (Vertex v in tempPolygon.vertices)
+            {
+                DrawVertex(v);
+            }
+
+            if (tempPolygon.vertices.Count > 1)
+                for (int i = 0; i < tempPolygon.vertices.Count - 1; i++)
+                {
+                    DrawEdge(p.vertices[i], p.vertices[i + 1]);
+                }
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            //using (Graphics g = Graphics.FromImage(drawArea))
+            //{
+            //    if (tempPolygon != null)
+            //    {
+            //        foreach (Vertex v in tempPolygon.vertices)
+            //        {
+            //            //rec.Location = new System.Drawing.Point(v.point.X - radius, v.point.Y - radius);
+            //            //g.FillEllipse(brushVertex, rec);
+            //            g.FillEllipse(brushVertex, v.point.X - radius, v.point.Y - radius, 2 * radius, 2 * radius);
+            //        }
+
+            //        if (tempPolygon.vertices.Count > 1)
+            //            for (int i = 0; i < tempPolygon.vertices.Count - 1; i++)
+            //            {
+            //                g.DrawLine(penEdge, tempPolygon.vertices[i].point, tempPolygon.vertices[i + 1].point);
+            //            }
+            //    }
+
+            //    if (polygons != null)
+            //        foreach (Polygon p in polygons)
+            //        {
+            //            foreach (Vertex v in p.vertices)
+            //            {
+            //                //rec.Location = new System.Drawing.Point(v.point.X - radius, v.point.Y - radius);
+            //                //g.FillEllipse(brushVertex, rec);
+            //                g.FillEllipse(brushVertex, v.point.X - radius, v.point.Y - radius, 2 * radius, 2 * radius);
+            //            }
+
+            //            for (int i = 0; i < p.vertices.Count; i++)
+            //            {
+            //                g.DrawLine(penEdge, p.vertices[i].point, p.vertices[(i + 1) % p.vertices.Count].point);
+            //            }
+            //        }
+            //}
+
+            if (tempPolygon != null)
+            {
+                if (tempPolygon.vertices.Count > 1)
+                    for (int i = 0; i < tempPolygon.vertices.Count - 1; i++)
+                    {
+                        e.Graphics.DrawLine(penEdgeHighlight, tempPolygon.vertices[i].point, tempPolygon.vertices[i + 1].point);
+                    }
+
+                foreach (Vertex v in tempPolygon.vertices)
+                {
+                    e.Graphics.FillEllipse(v.brush, v.point.X - radius, v.point.Y - radius, 2 * radius, 2 * radius);
+                }
+            }
+
+            if (polygons != null)
+                foreach (Polygon p in polygons)
+                {
+                    for (int i = 0; i < p.vertices.Count; i++)
+                    {
+                        e.Graphics.DrawLine(penEdge, p.vertices[i].point, p.vertices[(i + 1) % p.vertices.Count].point);
+                    }
+
+                    foreach (Vertex v in p.vertices)
+                    {
+                        e.Graphics.FillEllipse(v.brush, v.point.X - radius, v.point.Y - radius, 2 * radius, 2 * radius);
+                    }
+                }
+        }
+    }
+}
